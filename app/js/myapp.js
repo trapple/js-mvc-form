56e4f4d538ea560111d0b380437d87a644ef6dff
masuda <masuda@vivi-design.com>
2014-05-07 20:24:02 +0900

    普通のJS版

---
+++ app/js/myapp.js
@@ -0,0 +1,27 @@
+/*
+ * js-mvc-form
+ * Version: 1.0.0
+ */
+
+$(function() {
+  var $form = $(".todo-form");
+  var $input = $form.find("input[type=text]");
+  var $list = $(".todo-list");
+
+  $form.submit(function(e) {
+    e.preventDefault();
+
+    var text = $input.val();
+    if(text){
+      var html = '<li class="list-group-item"><input type="checkbox">　' + text + '</li>';
+      var $li = $(html);
+
+      $li.find("input[type='checkbox']").change(function() {
+        $(this).closest('li').toggleClass('list-group-item-info');
+      });
+      $list.append($li);
+      $input.val("");
+    }
+  });
+
+});
