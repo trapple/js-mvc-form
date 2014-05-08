23fd2b807338b53b55913e64edb99d7b553b70ad
masuda <masuda@vivi-design.com>
2014-05-08 15:00:33 +0900

    よく使うタスクの追加

--- app/js/myapp.js
+++ app/js/myapp.js
@@ -1,27 +1,37 @@
 /*
  * js-mvc-form
- * Version: 1.0.0
+ * Version: 1.1.0
  */
 
 $(function() {
   var $form = $(".todo-form");
   var $input = $form.find("input[type=text]");
   var $list = $(".todo-list");
+  var $usual = $(".usual-list");
 
-  $form.submit(function(e) {
+  function addList(text) {
+    var html = '<li class="list-group-item"><input type="checkbox">　' + text + '</li>';
+    var $li = $(html);
+
+    $li.find("input[type='checkbox']").change(function() {
+      $(this).closest('li').toggleClass('list-group-item-info');
+    });
+    $list.append($li);
+  }
+
+  $form.submit(function (e) {
     e.preventDefault();
-
     var text = $input.val();
     if(text){
-      var html = '<li class="list-group-item"><input type="checkbox">　' + text + '</li>';
-      var $li = $(html);
-
-      $li.find("input[type='checkbox']").change(function() {
-        $(this).closest('li').toggleClass('list-group-item-info');
-      });
-      $list.append($li);
-      $input.val("");
+      addList(text);
     }
+    $input.val("");
+  });
+
+  $usual.find('li').on('click', $usual, function (e) {
+    e.preventDefault();
+    var text = $(this).text();
+    addList(text);
   });
 
 });
